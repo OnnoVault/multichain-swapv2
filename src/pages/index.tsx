ca3d6b34d747d6ea31e13c7a54e91fc01e81ccb3
Onno Vault <156247001+OnnoVault@users.noreply.github.com>
2024-02-22 17:27:30 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export async function getStaticProps() {
 
 export default function Aggregator(props) {
 	return (
-		<Layout title={`Dexfin-Dex Aggregator`} defaultSEO>
+		<Layout title={`Onno Vault Aggregator`} defaultSEO>
 			<ConnectButton />
 			<AggregatorContainer tokenlist={props.tokenlist} />
 		</Layout>
