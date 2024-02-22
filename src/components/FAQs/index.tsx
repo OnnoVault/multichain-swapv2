6776ee18f4774e812d36f0e82b60973a2a11655c
Onno Vault <156247001+OnnoVault@users.noreply.github.com>
2024-02-22 17:39:37 -0300

    Update index.tsx

--- src/components/FAQs/index.tsx
+++ src/components/FAQs/index.tsx
@@ -3,6 +3,62 @@ import { TYPE } from '~/Theme';
 
 export default function FaqWrapper() {
 	return (
-		
+		<>
+			<TYPE.largeHeader>FAQ</TYPE.largeHeader>
+			<Accordion allowMultiple margin={'0 auto'} style={{ width: '100%', maxWidth: '600px' }}>
+				<AccordionItem>
+					<h2>
+						<AccordionButton>
+							<Box flex="1" textAlign="left">
+							
+							</Box>
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+				
+					</AccordionPanel>
+				</AccordionItem>
+
+				<AccordionItem>
+					<h2>
+						<AccordionButton>
+							<Box flex="1" textAlign="left">
+							
+							</Box>
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+						
+					</AccordionPanel>
+				</AccordionItem>
+				<AccordionItem>
+					<h2>
+						<AccordionButton>
+							<Box flex="1" textAlign="left">
+							
+							</Box>
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+						</AccordionPanel>
+				</AccordionItem>
+				<AccordionItem>
+					<h2>
+						<AccordionButton>
+							<Box flex="1" textAlign="left">
+						
+							</Box>
+							<AccordionIcon />
+						</AccordionButton>
+					</h2>
+					<AccordionPanel pb={4}>
+					
+					</AccordionPanel>
+				</AccordionItem>
+			</Accordion>
+		</>
 	);
 }
